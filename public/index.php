0000f42cffa45443ba740911f8370db01e18c22d
NvG8956XD02 <gutenburg429@gmail.com>
2023-03-21 10:05:20 +0100

    first commit

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Three.js Teszt</title>
+    <!-- Three.js CDN-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/0.150.1/three.min.js" integrity="sha512-pHIFefLE+ZH/TmpBDxPoDyC8kBxD773LqGShSv9VSiMFLluVRKKOFO9JA+TgnQruQEKovPtZ3vEz1Hwo/F4J2A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/0.150.1/three.js" integrity="sha512-+BaawenbN3MAXe8lP+ATJEZZLAuyVHYb9rxZKca05D42hw3a2xTBFkGSk5G11BFaEY4MfEwJcclw/Dk8CAjpyQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/0.150.1/three.module.js" integrity="sha512-2WnGzGRNe8eFctWDOVa4ixUUiAGyNhXWxN2K7YRr6Xro+Fv9EcRjyc78FgCmLNJvtznbBS3LvO68yqSraMYlIA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/0.150.1/three.module.min.js" integrity="sha512-1KFQITGie68q1Xv5aO8PyiH6iU0RZU2oSB+vRMbWGDiJhrA1h0DeRJiIp1/nuFv/e00ZdnK/l2sElvHRYvG9Cw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- Bootstrap CDN-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Saját CSS -->
+    <link rel="stylesheet" href="styles.css" />
+</head>
+<body>
+    <script src="./baseGame.js"></script>
+    
+    <div class="container-fluid bg-primary">
+        <div class="row">
+            
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ sql/start.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE teszt_db;
+USE teszt_db;
+
+CREATE TABLE teszt_tabla{
+    id  INTEGER,
+    nev TEXT
+};
